d0a097ce023526ffae9cecd71853fae97379dfc5
aml-youssef <66889985+aml-youssef@users.noreply.github.com>
2023-04-20 07:09:23 +0200

    encrypt elgamal

--- securitylibrary/ElGamal/ELGAMAL.cs
+++ securitylibrary/ElGamal/ELGAMAL.cs
@@ -16,10 +16,48 @@ namespace SecurityLibrary.ElGamal
         /// <param name="y"></param>
         /// <param name="k"></param>
         /// <returns>list[0] = C1, List[1] = C2</returns>
-        public List<long> Encrypt(int q, int alpha, int y, int k, int m)
+        public static int ModularPower(int baseVal, int expVal, int modVal)
         {
-            throw new NotImplementedException();
+            int initialVal = 1;
+            int i = 0;
+            int flag = 0;
+            baseVal = baseVal % modVal;
 
+            if (baseVal == 0)
+                return 0; // In case x is divisible by p;
+
+            if (expVal % 2 == 0)//even
+            {
+                expVal = expVal / 2;
+                flag = 1;
+            }
+            while (i < expVal)
+            {
+
+                initialVal = (initialVal * baseVal) % modVal;
+
+                i++;
+            }
+            if (flag == 1)
+            {
+                initialVal = (initialVal * initialVal) % modVal;
+            }
+
+
+            return initialVal;
+        }
+
+        public List<long> Encrypt(int q, int alpha, int y, int k, int m)
+        {   // q = mod
+            int K_ = ModularPower(y, k, q);
+            int c1 = ModularPower(alpha, k, q);
+            int c2 = (K_ * m) % q;
+
+            List<long> C = new List<long>();
+            C.Add((long)c1);
+            C.Add((long)c2);
+            return C;
+            //throw new NotImplementedException();
         }
         public int Decrypt(int c1, int c2, int x, int q)
         {
